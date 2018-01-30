31127a9566d2f60b523d615ad356010e7149ae98
MEstfeller <eschtlmax2@gmail.com>
2018-01-30 09:27:18 +0100

    lib

---
+++ src/sumo_parser/SumoToJSonParser.java
@@ -0,0 +1,60 @@
+package sumo_parser;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONStringer;
+import org.json.JSONTokener;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class SumoToJSonParser {
+
+    public static void main(String[] args) {
+        write("");
+        //parse(null, "C:\\Users\\User\\Desktop\\Parser\\Pattern.json");
+    }
+
+    private static void parse(File xmlFile, String JSonPath) {
+        JSONTokener jsonTokener;
+        try {
+            jsonTokener = new JSONTokener(new FileReader(JSonPath));
+            JSONObject jsonObject = new JSONObject(jsonTokener);
+            JSONArray nodes = ((JSONArray) jsonObject.get("nodes"));
+            JSONObject nodeEntry0 = nodes.getJSONObject(0);
+            String type = nodeEntry0.getString("type");
+
+            System.out.println(type);
+            System.out.println(nodeEntry0.toString());
+            System.out.println(nodes.toString());
+
+
+            //nodeEntry0.put("type", "Put");
+            //nodeEntry0.append("type", "Append");
+            //nodeEntry0.accumulate("type", "Accumulate");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void write(String path) {
+        /*
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("test", "pla");
+        jsonObject.accumulate("test2", "pla2");
+        JSONArray jsonArray = new JSONArray("[pla, pli, plu]");
+        jsonObject.append("testArray", "pla");
+        */
+
+        JSONObject jsonObject = new JSONObject();
+
+        JSONObject nodes_0 = new JSONObject();
+        nodes_0.put("id", "0");
+        nodes_0.put("x", 2);
+        jsonObject.put("nodes", nodes_0);
+
+        System.out.println(jsonObject.toString());
+    }
+}
